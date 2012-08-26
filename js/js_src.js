9141df797069dbe2ebecdd069cf29eb3b1814b53
Marcos Garcia de Lima <marckfree@gmail.com>
2012-08-26 19:30:15 -0300

    Melhorando a implementação utilizando um novo pattern

--- js/js_src.js
+++ js/js_src.js
@@ -2,31 +2,37 @@
    by Marcker - marckfree@gmail.com
    MIT License - < http://opensource.org/licenses/mit-license.php > */
 
-var d = document;
-var w = window;
-var floor = Math.floor;
-var random = Math.random;
-var urls = ['images/image_00.jpg',
-            'images/image_01.jpg', 
-            'images/image_02.jpg', 
-            'images/image_03.jpg', 
-            'images/image_04.jpg', 
+var Randomicimages = Randomicimages || {};
+
+Randomicimages = {
+    urls: (function(){
+        return [
+            'images/image_00.jpg',
+            'images/image_01.jpg',
+            'images/image_02.jpg',
+            'images/image_03.jpg',
+            'images/image_04.jpg',
             'images/image_05.jpg',
             'images/image_06.jpg'
-           ];
-var n_urls = urls.length;
-	
-function randomize_urls() {
-    if (n_urls) {
-        var randomized = floor(random()*n_urls);
-        return urls[randomized];
-    }
-    else {
-        return 'images/404.jpg';
-    }
-}
+        ];
+    })(),
 
-w.onload = function () {
-	    var image = document.getElementById('banner');
-	    image.src = randomize_urls();
-}
+    randomizeUrls: function() {
+        var urlsLength;
+        urlsLength = this.urls.length;
+
+        if (urlsLength) {
+            var randomized;
+            randomized = Math.floor(Math.random()*urlsLength);
+            return this.urls[randomized];
+        }
+        else {
+            return 'images/404.jpg';
+        }
+    }
+};
+
+window.onload = function() {
+    var image = document.getElementById('banner');
+    image.src = Randomicimages.randomizeUrls();
+};
